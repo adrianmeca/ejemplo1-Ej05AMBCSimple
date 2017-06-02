83792cd4738c1499db3fec0a06954a6ed8d9cd91
Adrian Meca <adrian.meca@avature.net>
2017-06-02 18:18:50 -0300

    ortografía

--- src/controlers/CtrlABMPersona.java
+++ src/controlers/CtrlABMPersona.java
@@ -31,7 +31,7 @@ public class CtrlABMPersona {
 		return this.pers.get(this.pers.indexOf(p));
 	}
 	
-	//este metodo me parece que debería quitarse
+	//este método me parece que debería quitarse
 	public Persona getByDni(String dni){
 		Persona p=new Persona();
 		p.setDni(dni);
